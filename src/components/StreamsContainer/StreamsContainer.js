b9f1315973596544b1b37da3b23a25ccb231e9bc
zksmith <zahiersmith96@gmail.com>
2020-06-12 01:35:02 -0400

    conditional rendering on streams container

--- src/components/StreamsContainer/StreamsContainer.js
+++ src/components/StreamsContainer/StreamsContainer.js
@@ -8,19 +8,24 @@ function StreamsContainer({ data }) {
     <>
       <h1>Twitch Streams</h1>
       <div className='streams-container'>
-        {data.map((stream) => {
-          console.log(stream);
-          const imageUrl = stream.thumbnail
-            .replace('{width}', '320')
-            .replace('{height}', '180');
-          return (
-            <StreamCard
-              thumbnail={imageUrl}
-              title={stream.title}
-              user={stream.user}
-              viewers={stream.viewers}
-            />
-          );
+        {data.map((stream, index) => {
+          // If statement to fix bug that caused app to crash if the user changes to "Twitch Streams" while posts are loading
+          if (stream.thumbnail) {
+            const imageUrl = stream.thumbnail
+              .replace('{width}', '320')
+              .replace('{height}', '180');
+
+            return (
+              <StreamCard
+                thumbnail={imageUrl}
+                title={stream.title}
+                user={stream.user}
+                viewers={stream.viewers}
+                key={index}
+              />
+            );
+          }
+          return null;
         })}
       </div>
     </>
